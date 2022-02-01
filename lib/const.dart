085718b39c406c60d7e71b6eb6f3898b786a45a3
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2022-02-01 20:38:42 +0530

    created constants file

---
+++ lib/const.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/painting.dart';
+
+const Color strongCyan = Color(0xff26c0ab);
+const Color veryDarkCyan = Color(0xff00494d);
+const Color darkGrayishCyan = Color(0xff5e7a7d);
+const Color lightGrayishCyan = Color(0xffc5e4e7);
+const Color white = Color(0xffffffff);
+
