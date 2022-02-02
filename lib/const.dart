088a687e6835e6e728a3e7a8e866d43da8edac3a
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2022-02-02 22:09:03 +0530

    added new color

--- lib/const.dart
+++ lib/const.dart
@@ -4,5 +4,6 @@ const Color strongCyan = Color(0xff26c0ab);
 const Color veryDarkCyan = Color(0xff00494d);
 const Color darkGrayishCyan = Color(0xff5e7a7d);
 const Color lightGrayishCyan = Color(0xffc5e4e7);
+const Color whitishCyan = Color(0xfff4fafa);
 const Color white = Color(0xffffffff);
 
