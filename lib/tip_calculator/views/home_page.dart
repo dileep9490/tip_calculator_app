e88591452fa1734da32671e7587f66f924cdb9dc
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2022-02-04 18:43:13 +0530

    made bill from widget

--- lib/tip_calculator/views/home_page.dart
+++ lib/tip_calculator/views/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
         backgroundColor: lightGrayishCyan,
         body: _HomePage(),
@@ -17,10 +17,13 @@ class HomePage extends StatelessWidget {
 }
 
 class _HomePage extends StatelessWidget {
-  const _HomePage({Key? key}) : super(key: key);
+  _HomePage({Key? key}) : super(key: key);
+
+  final TextEditingController billValue = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    billValue.text = '123';
     const Radius containerRadius = Radius.circular(20);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -31,13 +34,57 @@ class _HomePage extends StatelessWidget {
           child: SvgPicture.asset('assets/images/logo.svg'),
         ),
         Container(
+          width: double.infinity,
           height: 400,
           decoration: const BoxDecoration(
               borderRadius: BorderRadius.only(
                   topLeft: containerRadius, topRight: containerRadius),
               color: Colors.white),
-          child: const Center(
-            child: Text("hi"),
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Bill',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(fontSize: 18, color: darkGrayishCyan),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      prefixIconConstraints: const BoxConstraints(
+                          maxHeight: 20,
+                          maxWidth: 20,
+                          minHeight: 20,
+                          minWidth: 20),
+                      prefixIcon: SvgPicture.asset(
+                        'assets/images/icon-dollar.svg',
+                        height: 8,
+                        width: 8,
+                      ),
+                      border:
+                          const OutlineInputBorder(borderSide: BorderSide.none),
+                      fillColor: whitishCyan,
+                      filled: true,
+                      // hintText: '123',
+                    ),
+                    style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                          fontSize: 22,
+                          color: veryDarkCyan,
+                        ),
+                    controller: billValue,
+                    textAlign: TextAlign.right,
+                    keyboardType: TextInputType.number,
+                  )
+                ],
+              ),
+            ),
           ),
         )
       ],
