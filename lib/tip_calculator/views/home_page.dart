9d56fad0f65070e141532b1a8706ded9e06ce4f7
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2022-02-06 11:51:51 +0530

    UI creation completed

--- lib/tip_calculator/views/home_page.dart
+++ lib/tip_calculator/views/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:tip_calculator_app/const.dart';
+import 'package:tip_calculator_app/tip_calculator/widgets/form_widget.dart';
+import 'package:tip_calculator_app/tip_calculator/widgets/button_widget.dart';
+import 'package:tip_calculator_app/tip_calculator/widgets/custom_button.dart';
+
+import '../widgets/result_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,84 +15,104 @@ class HomePage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: lightGrayishCyan,
-        body: _HomePage(),
+        body: _HomePageView(),
       ),
     );
   }
 }
 
-class _HomePage extends StatelessWidget {
-  _HomePage({Key? key}) : super(key: key);
+class _HomePageView extends StatelessWidget {
+  _HomePageView({Key? key}) : super(key: key);
 
-  final TextEditingController billValue = TextEditingController();
-
+  final TextEditingController billcontroller = TextEditingController();
+  final TextEditingController customController = TextEditingController();
+  final TextEditingController peopleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    billValue.text = '123';
+    billcontroller.text = '0.00';
+    peopleController.text = '0';
     const Radius containerRadius = Radius.circular(20);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 50),
-          alignment: Alignment.center,
-          child: SvgPicture.asset('assets/images/logo.svg'),
-        ),
-        Container(
-          width: double.infinity,
-          height: 400,
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topLeft: containerRadius, topRight: containerRadius),
-              color: Colors.white),
-          child: Padding(
-            padding: const EdgeInsets.all(24),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Bill',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText1!
-                        .copyWith(fontSize: 18, color: darkGrayishCyan),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      prefixIconConstraints: const BoxConstraints(
-                          maxHeight: 20,
-                          maxWidth: 20,
-                          minHeight: 20,
-                          minWidth: 20),
-                      prefixIcon: SvgPicture.asset(
-                        'assets/images/icon-dollar.svg',
-                        height: 8,
-                        width: 8,
-                      ),
-                      border:
-                          const OutlineInputBorder(borderSide: BorderSide.none),
-                      fillColor: whitishCyan,
-                      filled: true,
-                      // hintText: '123',
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 50),
+            alignment: Alignment.center,
+            child: SvgPicture.asset('assets/images/logo.svg'),
+          ),
+          Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: containerRadius, topRight: containerRadius),
+                color: Colors.white),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FormWidget(
+                      title: 'Bill',
+                      controller: billcontroller,
+                      prefixIconPath: 'assets/images/icon-dollar.svg',
                     ),
-                    style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                          fontSize: 22,
-                          color: veryDarkCyan,
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Select Tip',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(fontSize: 18, color: darkGrayishCyan),
                         ),
-                    controller: billValue,
-                    textAlign: TextAlign.right,
-                    keyboardType: TextInputType.number,
-                  )
-                ],
+                        Table(
+                          children: [
+                            const TableRow(
+                              children: [
+                                PercentageButton(title: '5%'),
+                                PercentageButton(title: '10%')
+                              ],
+                            ),
+                            const TableRow(
+                              children: [
+                                PercentageButton(title: '15%'),
+                                PercentageButton(title: '25%')
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                const PercentageButton(title: '50%'),
+                                CustomButton(
+                                  customController: customController,
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        FormWidget(
+                          title: 'Number of People',
+                          controller: peopleController,
+                          prefixIconPath: 'assets/images/icon-person.svg',
+                        ),
+                       const ResultWidget()
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
